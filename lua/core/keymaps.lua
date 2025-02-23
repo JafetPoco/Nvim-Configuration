3a506ed11e3f9049ca254c80e2a7815e8f693589
JafetPoco <jpococ@unsa.edu.pe>
2025-02-23 18:45:13 -0500

    Mapeos de teclas básicos

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "     --Define la tecla lider como espacio
+vim.keymap.set("n", "<leader>w", ":w<CR>")  --Guarda con <leader>w
+vim.keymap.set("n", "<leader>q", ":q<CR>")  --Salir con <leader>q
